63e3f41e7a20e1b7e21e3abb4a2354e29a9aff21
kholifpra9 <kwalid12355@gmail.com>
2024-01-02 21:24:26 +0700

    tambah panel pengecekan

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('gudang.index');
+    return view('owner.index');
 });
 
 Route::get('/dashboard', function () {
