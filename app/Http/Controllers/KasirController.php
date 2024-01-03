3451105ce5c9347f7e57125afbf1729fd926956d
kholifpra9 <kwalid12355@gmail.com>
2024-01-03 14:18:57 +0700

    fixed route + view Controller

--- app/Http/Controllers/KasirController.php
+++ app/Http/Controllers/KasirController.php
@@ -11,7 +11,7 @@ class KasirController extends Controller
      */
     public function index()
     {
-        //
+        return view('kasir.index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\GudangController;
+use App\Http\Controllers\KasirController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -37,45 +39,30 @@ Route::middleware('auth')->group(function () {
             'middleware' => ['role:owner'],
         ],
         function () {
-            Route::get('/kasir', [UserController::class, 'transaksiKasir'])->name('kasir.transaksiKasir');
+            Route::group(['middleware' => ['role:owner|manajer'],], function () {
+            
+            });
+
+            Route::group(['middleware' => ['role:owner|manajer|supervisor'],],function () {
+            
+            });
+
+            Route::group(['middleware' => ['role:kasir|owner'],],function () {
+                Route::get('/kasir', [KasirController::class, 'index'])->name('kasir.index');
+            });
+
+            Route::group(['middleware' => ['role:pegawai gudang|owner|manajer'],],function () {
+                Route::get('/gudang', [GudangController::class, 'index'])->name('gudang.index');
+            });
         }
     );
 
-    Route::group(
-        [
-            'middleware' => ['role:manajer'],
-        ],
-        function () {
-            
-        }
-    );
+    
 
-    Route::group(
-        [
-            'middleware' => ['role:supervisor'],
-        ],
-        function () {
-            
-        }
-    );
+    
 
-    Route::group(
-        [
-            'middleware' => ['role:kasir'],
-        ],
-        function () {
-            
-        }
-    );
 
-    Route::group(
-        [
-            'middleware' => ['role:pegawai gudang'],
-        ],
-        function () {
-            
-        }
-    );
+    
 
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
