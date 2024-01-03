f64e0c8fd5a42f07c12c7e3234cfe1722bb12732
kholifpra9 <kwalid12355@gmail.com>
2024-01-03 14:02:23 +0700

    setting route role + add controller

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -66,6 +66,7 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(RouteServiceProvider::HOME);
+        // return redirect(RouteServiceProvider::HOME);
+        return redirect('dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,11 +20,64 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('owner.index');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::group(['middleware' => ['role:owner|manajer']], function () {
+    Route::get('/dashboard', function () {
+        return view('owner.index');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+});
+
 
 Route::middleware('auth')->group(function () {
+    Route::group(
+        [
+            'middleware' => ['role:owner'],
+        ],
+        function () {
+            Route::get('/kasir', [UserController::class, 'transaksiKasir'])->name('kasir.transaksiKasir');
+        }
+    );
+
+    Route::group(
+        [
+            'middleware' => ['role:manajer'],
+        ],
+        function () {
+            
+        }
+    );
+
+    Route::group(
+        [
+            'middleware' => ['role:supervisor'],
+        ],
+        function () {
+            
+        }
+    );
+
+    Route::group(
+        [
+            'middleware' => ['role:kasir'],
+        ],
+        function () {
+            
+        }
+    );
+
+    Route::group(
+        [
+            'middleware' => ['role:pegawai gudang'],
+        ],
+        function () {
+            
+        }
+    );
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
